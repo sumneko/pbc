f16eda81b412a2c62319084a0de952dec0ee8639
最萌小汐 <sumneko@hotmail.com>
2019-10-31 10:28:52 +0800

    构建

---
+++ make.lua
@@ -0,0 +1,38 @@
+local lm = require 'luamake'
+local platform = require "bee.platform"
+
+lm.arch = 'x64'
+
+lm:import '3rd/bee.lua/make.lua'
+
+lm:shared_library 'pbc-lua' {
+    deps = {
+        platform.OS == "Windows" and "lua54" or "lua",
+    },
+    sources = {
+        'binding/lua53/pbc-lua53.c',
+        'src/*.c',
+    },
+    includes = {
+        'src',
+        '.'
+    },
+    flags = {
+        '/TP',
+        '/wd"4146"',
+        '/wd"4273"',
+        '/wd"4244"',
+        '/wd"4018"',
+        '/wd"4267"',
+    },
+    defines = {
+        'NDEBUG',
+        '_UNICODE',
+        'UNICODE',
+    },
+    visibility = 'default',
+}
+
+lm:default {
+    'pbc-lua',
+}
